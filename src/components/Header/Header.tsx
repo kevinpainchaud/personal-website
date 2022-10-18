e7607bc999b6eb42fb6629139c18055cd6befcd6
Kévin Painchaud <mail@kpcd.me>
2022-10-18 21:27:18 +0200

    fix(Header): add title attribute to links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,11 @@ export const Header = () => (
   <div className="header flex justify-end py-6 md:py-8">
     <ul className="flex gap-4">
       <li>
-        <a href="https://github.com/kevinpainchaud" target="_blank">
+        <a
+          href="https://github.com/kevinpainchaud"
+          target="_blank"
+          title="GitHub"
+        >
           <IoLogoGithub size={24} />
         </a>
       </li>
@@ -15,12 +19,17 @@ export const Header = () => (
         <a
           href="https://www.linkedin.com/in/kevin-painchaud-a2605935/"
           target="_blank"
+          title="Linkedin"
         >
           <IoLogoLinkedin size={24} />
         </a>
       </li>
       <li>
-        <a href="https://twitter.com/kevinpainchaud" target="_blank">
+        <a
+          href="https://twitter.com/kevinpainchaud"
+          target="_blank"
+          title="Twitter"
+        >
           <IoLogoTwitter size={24} />
         </a>
       </li>
